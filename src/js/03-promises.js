0a8eb3bf9999cb2f819ffac68803fc49f19e9559
natalilia <nata.semenii@gmail.com>
2023-11-27 15:23:41 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,49 @@
 import Notiflix from 'notiflix';
 
+const form = document.querySelector('.form');
 
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+
+  const firstDelay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  let currentDelay = firstDelay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+        );
+      });
+
+    currentDelay += step;
+  }
+  form.reset();
+}
+
