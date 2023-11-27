7a05f716e50b98f08442976c7e451b90d7345f76
natalilia <nata.semenii@gmail.com>
2023-11-27 15:12:16 +0100

    z

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -28,59 +28,59 @@ const refs = {
     return { days, hours, minutes, seconds };
   }
 
-
-
+  
   function addLeadingZero(value) {
     return String(value).padStart(2, "0");
-  }const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const currentDate = new Date();
-    const selectedDate = selectedDates[0];
-
-    if (selectedDate <= currentDate) {
-      Notiflix.Notify.failure("Please choose a date in the future");
-      refs.startBtn.setAttribute("disabled", "");
-    } else {
-      refs.startBtn.removeAttribute("disabled");
-    }
-  },
-};
-
-const fp = flatpickr("#datetime-picker", options);
-
-let timerId = null;
-
-refs.startBtn.addEventListener("click", () => {
-  const selectedDate = fp.selectedDates[0];
-  const currentDate = new Date();
-
-  if (selectedDate > currentDate) {
-    timerId = setInterval(() => {
-      const selectedDate = fp.selectedDates[0];
-      const currentDate = new Date();
-      const timeDifference = selectedDate - currentDate;
-
-      const { days, hours, minutes, seconds } = convertMs(timeDifference);
-
-      refs.days.textContent = addLeadingZero(days);
-      refs.hours.textContent = addLeadingZero(hours);
-      refs.minutes.textContent = addLeadingZero(minutes);
-      refs.seconds.textContent = addLeadingZero(seconds);
-
-      if (timeDifference <= 0) {
-        clearInterval(timerId);
-        Notiflix.Notify.success("Countdown finished!");
-        refs.days.textContent = "00";
-        refs.hours.textContent = "00";
-        refs.minutes.textContent = "00";
-        refs.seconds.textContent = "00";
-      }
-    }, 1000);
-  } else {
-    Notiflix.Notify.warning("You chose the same date");
   }
-});
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      const currentDate = new Date();
+      const selectedDate = selectedDates[0];
+  
+      if (selectedDate <= currentDate) {
+        Notiflix.Notify.failure("Please choose a date in the future");
+        refs.startBtn.setAttribute("disabled", "");
+      } else {
+        refs.startBtn.removeAttribute("disabled");
+      }
+    },
+  };
+  
+  const fp = flatpickr("#datetime-picker", options);
+  
+  let timerId = null;
+  
+  refs.startBtn.addEventListener("click", () => {
+    const selectedDate = fp.selectedDates[0];
+    const currentDate = new Date();
+  
+    if (selectedDate > currentDate) {
+      timerId = setInterval(() => {
+        const selectedDate = fp.selectedDates[0];
+        const currentDate = new Date();
+        const timeDifference = selectedDate - currentDate;
+  
+        const { days, hours, minutes, seconds } = convertMs(timeDifference);
+  
+        refs.days.textContent = addLeadingZero(days);
+        refs.hours.textContent = addLeadingZero(hours);
+        refs.minutes.textContent = addLeadingZero(minutes);
+        refs.seconds.textContent = addLeadingZero(seconds);
+  
+        if (timeDifference <= 0) {
+          clearInterval(timerId);
+          Notiflix.Notify.success("Countdown finished!");
+          refs.days.textContent = "00";
+          refs.hours.textContent = "00";
+          refs.minutes.textContent = "00";
+          refs.seconds.textContent = "00";
+        }
+      }, 1000);
+    } else {
+      Notiflix.Notify.warning("You chose the same date");
+    }
+  });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+import Notiflix from 'notiflix';
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
